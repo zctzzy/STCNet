106f687adf7a70f3d8f367734721e8aaec4a03cc
Chuanting Zhang <Chuanting.Zhang@gmail.com>
2018-07-30 17:29:57 +0800

    add source scripts

---
+++ JSAC/FinalProject/sripts/TransferTest/pretrained/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+/*******************************************
+** This is a file created by Chuanting Zhang
+** Name: __init__.py
+** Date: 5/18/18
+** Email: chuanting.zhang@gmail.com
+** BSD license
+********************************************/
+"""
